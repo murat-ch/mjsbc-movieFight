7d6b34a359eb62460b628244e202c85502ff06dd
murat-ch <muratik.ch@gmail.com>
2020-11-15 02:30:28 -0500

    1 - fetch movies

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '74bc5178',
+            s: searchTerm
+        }
+    });
+    if (response.Error) {
+        return [];
+    }
+    return response.data.Search;
+}
+const input = document.querySelector('input');
+
+
+
+const onInput = async (event) => {
+   const movies = await fetchData(event.target.value);
+   for (let movie of movies) {
+       const div = document.createElement('div');
+
+       div.innerHTML = `
+       <img src="${movie.Poster}"/>
+       <h1>${movie.Title}</h1>
+       `;
+       document.querySelector('#target').appendChild(div);
+   }
+};
+
+input.addEventListener('input', debounce(onInput, 500));
